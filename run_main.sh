a8bead14a418c6d71de4f7f0c49429ea296b7c3c
Dennis Hollanders <dennis.hollanders17@gmail.com>
2024-03-08 14:47:32 +0100

    change directory run_main

--- run_main.sh
+++ run_main.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
+#SBATCH --job-name="first_attempt"
 #SBATCH --nodes=1
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=18
 #SBATCH --gpus=1
 #SBATCH --partition=gpu
-#SBATCH --time=1:00:00
+#SBATCH --time=0:01:00
 
-cd /gpfs/home5/scur0761/5LSM0-Neural-Networks-for-Computer-Vision/
+cd /gpfs/home5/scur0761/5LSM0-Neural-Networks-for-Computer-Vision
 mkdir wandb/$SLURM_JOBID
 
 srun apptainer exec --nv /gpfs/work5/0/jhstue005/JHS_data/5lsm0_v1.sif /bin/bash run_container.sh
